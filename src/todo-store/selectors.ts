f1f646e48aab606ab0d5c1866655a3d4421d4c03
Tomer <tomer.bu@gmail.com>
2024-07-01 17:28:29 +0300

    added getTodoCount, egtCompletedTodos, getPendingTodos

--- src/todo-store/selectors.ts
+++ src/todo-store/selectors.ts
@@ -2,6 +2,11 @@ import { State } from "../types";
 
 const selectors = {
   getTodos: (state: State) => state.todos,
+  getTodoCount: (state: State) => state.todos.length,
+  getCompletedTodos: (state: State) =>
+    state.todos.filter((todo) => todo.completed),
+  getPendingTodos: (state: State) =>
+    state.todos.filter((todo) => !todo.completed),
   getTodoById: (state: State, id: number) =>
     state.todos.find((todo) => todo.id === id),
 };
